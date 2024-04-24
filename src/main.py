b53c02afcd5da0882d82729115b67e8985fb5471
lucamaiano <lucamaiano@gmail.com>
2024-04-24 15:28:05 +0200

    Hydra support

--- src/main.py
+++ src/main.py
@@ -10,56 +10,80 @@ from torchvision import transforms
 
 import data_setup, engine, model_builder, utils
 
+# Adding Hydra support
+import logging
 
-# Setup hyperparameters
-NUM_EPOCHS = 5
-BATCH_SIZE = 32
-HIDDEN_UNITS = 10
-LEARNING_RATE = 0.001
+import hydra
+from omegaconf import DictConfig
+from omegaconf import OmegaConf
 
-# Setup directories
-train_dir = "data/pizza_steak_sushi/train"
-test_dir = "data/pizza_steak_sushi/test"
+from hydra.utils import to_absolute_path
 
-# Setup target device
-device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Create transforms
-data_transform = transforms.Compose([
-  transforms.Resize((64, 64)),
-  transforms.ToTensor()
-])
+# Logger
+log = logging.getLogger(__name__)
 
-# Create DataLoaders with help from data_setup.py
-train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
-    train_dir=train_dir,
-    test_dir=test_dir,
-    transform=data_transform,
-    batch_size=BATCH_SIZE
-)
 
-# Create model with help from model_builder.py
-model = model_builder.TinyVGG(
-    input_shape=3,
-    hidden_units=HIDDEN_UNITS,
-    output_shape=len(class_names)
-).to(device)
+def train(conf: DictConfig) -> None:
+    # Setup hyperparameters
+    NUM_EPOCHS = conf.train.num_epochs
+    BATCH_SIZE = conf.train.batch_size
+    HIDDEN_UNITS = conf.train.hidden_hunits
+    LEARNING_RATE = conf.train.learning_rate
 
-# Set loss and optimizer
-loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(),
-                             lr=LEARNING_RATE)
+    # Setup directories
+    train_dir = to_absolute_path(conf.dataset.train_dir)
+    test_dir = to_absolute_path(conf.dataset.test_dir)
 
-# Start training with help from engine.py
-engine.train(model=model,
-             train_dataloader=train_dataloader,
-             test_dataloader=test_dataloader,
-             loss_fn=loss_fn,
-             optimizer=optimizer,
-             epochs=NUM_EPOCHS,
-             device=device)
+    # Setup target device
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Save the model with help from utils.py
-utils.save_model(model=model,
-                 target_dir="models",
-                 model_name="05_going_modular_script_mode_tinyvgg_model.pth")
+    # Create transforms
+    data_transform = transforms.Compose(
+        [transforms.Resize((64, 64)), transforms.ToTensor()]
+    )
+
+    # Create DataLoaders with help from data_setup.py
+    train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+        train_dir=train_dir,
+        test_dir=test_dir,
+        transform=data_transform,
+        batch_size=BATCH_SIZE,
+    )
+
+    # Create model with help from model_builder.py
+    model = model_builder.TinyVGG(
+        input_shape=3, hidden_units=HIDDEN_UNITS, output_shape=len(class_names)
+    ).to(device)
+
+    # Set loss and optimizer
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    # Start training with help from engine.py
+    engine.train(
+        model=model,
+        train_dataloader=train_dataloader,
+        test_dataloader=test_dataloader,
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+        epochs=NUM_EPOCHS,
+        device=device,
+    )
+
+    # Save the model with help from utils.py
+    utils.save_model(
+        model=model,
+        target_dir=to_absolute_path("models"),
+        model_name=conf.train.model_name,
+    )
+
+
+@hydra.main(config_path="../conf", config_name="config")
+def main(conf: DictConfig):
+    log.info(OmegaConf.to_yaml(conf))
+    train(conf)
+
+
+if __name__ == "__main__":
+    main()
